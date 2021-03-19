e73bf2ab0092e7b662b70b76c4845bf1cfddb545
kiinyo <iiyoheki@gmail.com>
2021-03-18 21:10:59 -0700

    yet another overhaul

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,118 @@
-use std::sync::{ Arc, Mutex };
+pub const CHECK: u128 = 170141183460469231731687303715884105728;
 
-pub struct Pins {
-	pre: u128,             // Previous pin value
-	pub val: u128,             // Current pin value
-	bus: Arc<Mutex<u128>>, // Bus pins are connected to
-	pub con: [usize;128],      // List of all the connections
-	len: usize             // How many pins are used
+/// A collection of bit addresses stored as a single u128
+pub trait State {
+	
+	// Accessing locations
+	
+	/// Return the state of an index
+	fn get_index ( &mut self, index: usize ) -> bool;
+	/// Set the state of an index.
+	/// (returns the previous state)
+	fn set_index ( &mut self, index: usize, on: bool ) -> bool;
+	/// Inverts the state of an index.
+	/// (returns the previous state)
+	fn inv_index ( &mut self, index: usize ) -> bool;
+	
+	// Dealing with the states as a whole
+	
+	/// Returns a number of contiguous indexes represented as a u128
+	fn get_state ( &mut self, index: usize, length: usize ) -> u128;	
+	/// Sets a number of contiguous indexes represented as a u128.
+	/// (returns previous states of the indexes represented as a u128)
+	fn set_state ( &mut self, index: usize, length: usize, value: u128 ) -> u128;
+	/// Flips every bit of a contiguous indexes
+	/// (returns previous states of the indexes represented as a u128)
+	fn inv_state ( &mut self, index: usize, length: usize ) -> u128;
+	
 }
 
-impl Pins {
-	// Get the on/off of the pin
-	fn get (&self, pin: usize) -> bool {
-		170141183460469231731687303715884105728 <= self.val << 127 - pin
+pub struct Register {
+	state: u128
+}
+
+impl State for Register {
+	fn get_index ( &mut self, index: usize ) -> bool {
+		self.state << index >= CHECK
 	}
-	// Set the on/off of the pin
-	fn set (&mut self, pin: usize, on: bool) {
-		if 170141183460469231731687303715884105728 <= self.val << 127 - pin {
-			// The pin is on
+	fn set_index ( &mut self, index: usize, on: bool ) -> bool {
+		if self.state << index >= CHECK {
+			// Index is on
 			if on {
-				// Do nothing, it's on
+				// Do nothing
 			} else {
-				// Flip it
-				self.val ^= 1_u128 << pin;
+				self.state ^= CHECK >> index;
 			}
+			return true;
 		} else {
-			// The pin is off
+			// Index is off
 			if on {
-				// Flip it
-				self.val |= 1_u128 << pin;
+				self.state |= CHECK >> index;
 			} else {
 				// Do nothing
 			}
+			return false;
 		}
 	}
-	fn refresh (&mut self) {
-		// Get changes
-		let cha = self.val ^ self.pre;
-		let mut b = self.bus.lock().unwrap();
-		// Refresh pins
-		for x in 0..self.len {
-			// We're writing a change
-			if 170141183460469231731687303715884105728 <= cha << 127 - x {
-				// A change has occurred on this pin
-				if self.get(x) {
-					// The change is On
-					if *b << 127 - self.con[x] >= 170141183460469231731687303715884105728 {
-						// Do nothing, the bus is already on
-					} else {
-						*b |= 1_u128 << self.con[x]
-					}
+	fn inv_index ( &mut self, index: usize ) -> bool {
+		let boolean = !self.get_index(index);
+		self.set_index( index, boolean )
+	}
+	
+	fn get_state ( &mut self, index: usize, length: usize ) -> u128 {
+		let mut previous: u128 = 0;
+		for x in 0..length {
+			if self.state << 127 - x - index >= CHECK {
+				// The original index is on				
+				previous += 1u128 << x;
+			} else {
+				// The original index is off
+			}
+		}
+		return previous;
+	}
+	fn set_state ( &mut self, index: usize, length: usize, value: u128 ) -> u128 {
+		let mut previous: u128 = 0;
+		for x in 0..length {
+			if self.state << x + index >= CHECK {
+				// The original index is on				
+				previous += 1u128 << x;
+				if value << 127 - x - index >= CHECK {
+					// The new index is on
+					// Do nothing
 				} else {
-					// The change is Off
-					if *b << 127 - self.con[x] >= 170141183460469231731687303715884105728 {
-						*b ^= 1_u128 << self.con[x]
-					} else {
-						// Do nothing, the bus is already off
-					}					
+					// The new value is off
+					self.state ^= 1u128 << 127 - x - index;
+				}
+			} else {
+				// The original index is off
+				if value << 127 - x >= CHECK {
+					// The new index is on
+					self.state |= 1u128 << 127 - x - index;
+				} else {
+					// The new value is off
+					// do nothing
 				}
 			}
-			// We're updating our pins
-			else {
-				if self.get(x) {
-					// Our pin is on
-					if *b << 127 - self.con[x] >= 170141183460469231731687303715884105728 {
-						// Do nothing, the bus is on too
-					} else {
-						self.val ^= 1_u128 << x;
-					}
-				} else {
-					// Our pin is off
-					if *b << 127 - self.con[x] >= 170141183460469231731687303715884105728 {
-						self.val |= 1_u128 << x;
-					} else {
-						// Do nothing, the bus is off too
-					}					
-				}
-			}			
 		}
-		drop(b);
-		self.pre = self.val;
+		return previous;
 	}
+	fn inv_state ( &mut self, index: usize, length: usize ) -> u128 {
+		let mut previous: u128 = 0;
+		for x in 0..length {
+			if self.state << 127 - x - index >= CHECK {
+				// The original index is on				
+				previous += 1u128 << x;
+				self.state ^= 1u128 << 127 - x - index;
+			} else {
+				// The original index is off
+				self.state |= 1u128 << 127 - x - index;
+			}
+		}
+		return previous;
+	}		
 }
 
 fn main () {
-	let mut bus = Arc::new(Mutex::new(5)); // ...101
-	let mut pins = Pins { // ...000
-		pre: 0,
-		val: 0,
-		bus: Arc::clone(&bus),
-		con: [0; 128],
-		len: 3
-	};
-	pins.con[0] = 0;
-	pins.con[1] = 1;
-	pins.con[2] = 2;
-	
-	println!("Pins: {}", pins.val);
-	
-	pins.refresh();
-	
-	println!("Pins: {}", pins.val); // ...101
-	
-	pins.set(1, true); // ...111
-	let mut b = bus.lock().unwrap();
-	*b = 0;
-	drop(b); // ...000
-	pins.refresh();
-	
-	let mut b = *bus.lock().unwrap();
-	
-	println!("Bus: {}", b); // ...010
-	
-		
+	println! ("Heyo!");		
 }
