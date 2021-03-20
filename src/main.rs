7a48d97a737352fdcd8fecf1a1738ed86609f5c6
kiinyo <iiyoheki@gmail.com>
2021-03-19 22:21:28 -0700

    added unsafe connectedregister

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub const CHECK: u128 = 170141183460469231731687303715884105728;
 
 /// A collection of bit addresses stored as a single u128
+
 pub trait State {
 	
 	// Accessing locations
@@ -27,6 +28,7 @@ pub trait State {
 	
 }
 
+/// A simple Register that has 128 addresses to store a bit
 pub struct Register {
 	state: u128
 }
@@ -61,8 +63,8 @@ impl State for Register {
 	
 	fn get_state ( &mut self, index: usize, length: usize ) -> u128 {
 		let mut previous: u128 = 0;
-		for x in 0..length {
-			if self.state << 127 - x - index >= CHECK {
+		for x in 0..=length {
+			if self.state << x - index >= CHECK {
 				// The original index is on				
 				previous += 1u128 << x;
 			} else {
@@ -113,6 +115,113 @@ impl State for Register {
 	}		
 }
 
-fn main () {
-	println! ("Heyo!");		
+/// A shared Register using mutable reference to a u128
+/// ( unsafe! )
+pub struct ConnectedRegister<'a> {
+	pub state: &'a mut u128,
+	pub con: [usize; 128]	
 }
+
+impl State for ConnectedRegister<'_> {
+	// Single indexing
+	fn get_index ( &mut self, index: usize ) -> bool {
+		let x = self.con[index];
+		*self.state << x >= CHECK
+	}
+	fn set_index ( &mut self, index: usize, on: bool ) -> bool {
+		let x = self.con[index];
+		if *self.state << index >= CHECK {
+			// Index is on
+			if on {
+				// Do nothing
+			} else {
+				*self.state ^= CHECK >> x;
+			}
+			return true;
+		} else {
+			// Index is off
+			if on {
+				*self.state |= CHECK >> x;
+			} else {
+				// Do nothing
+			}
+			return false;
+		}
+	}
+	fn inv_index ( &mut self, index: usize ) -> bool {
+		let boolean = !self.get_index(index);
+		self.set_index( index, boolean )
+	}
+	// Group indexing
+	fn get_state ( &mut self, index: usize, length: usize ) -> u128 {
+		let mut previous: u128 = 0;
+		for x in 0..=length {
+			if self.get_index(index + x) {
+				// The original index is on				
+				previous += 1u128 << x;
+			} else {
+				// The original index is off
+			}
+		}
+		return previous;
+	}
+	fn set_state ( &mut self, index: usize, length: usize, value: u128 ) -> u128 {
+		let mut previous: u128 = 0;
+		for x in 0..length {
+			if self.get_index(x + index) {
+				// The original index is on				
+				previous += 1u128 << x;
+				if value << 127 - x - index >= CHECK {
+					// The new index is on
+					// Do nothing
+				} else {
+					// The new value is off
+					self.set_index(x + index, false);
+				}
+			} else {
+				// The original index is off
+				if value << 127 - x - index >= CHECK {
+					// The new index is on
+					self.set_index(x + index, true);					
+				} else {
+					// The new value is off
+					// do nothing
+				}
+			}
+		}
+		return previous;
+	}
+	fn inv_state ( &mut self, index: usize, length: usize ) -> u128 {
+		let mut previous: u128 = 0;
+		for x in 0..length {
+			if self.inv_index( index + x ) {		
+				previous += 1u128 << x;
+			}
+		}
+		return previous;
+	}
+}
+		
+	
+	
+fn main () {
+	println! ("Heyo!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
